d8a3c5a8d30f793ac3b72b2d8be583fe8a06e9cf
shubhraagarwal <agarwalshubhra2002@gmail.com>
2021-04-11 11:16:15 +0530

    Q30 Absolute Beginner

--- AbsoluteBeginner/Q30.cpp
+++ AbsoluteBeginner/Q30.cpp
@@ -1,13 +1,19 @@
-#include<iostream>
-using namespace std ;
-
-int main(){
-    string str;
-    getline(cin, str);
-    for (int i = str.length() ; i >= 0 ; i--){
-        char c = str[i];
-        cout<<c ;
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    int n = s.length();
+    int start = 0, end = n - 1;
+    while (start < end)
+    {
+        s[start] ^= s[end];
+        s[end] ^= s[start];
+        s[start] ^= s[end];
+        start++;
+        end--;
     }
-
+    cout << s << endl;
     return 0;
 }
