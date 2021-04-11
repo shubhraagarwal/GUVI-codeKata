8d94921a4ee42f590b758581e9acbaf5eb9076a9
shubhraagarwal <agarwalshubhra2002@gmail.com>
2021-04-11 11:41:00 +0530

    Q26 Absolute Beginner

---
+++ AbsoluteBeginner/Q26.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int A, N;
+    cin>>A;
+    cin>>N;
+    for(int i = 1 ; i<=N ; i++){
+        cout<<A<<endl;
+    }
+    return 0;
+
+}
