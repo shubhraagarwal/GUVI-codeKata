a6f22bbce312411ff11688495631760966685d14
shubhraagarwal <agarwalshubhra2002@gmail.com>
2021-04-17 12:26:16 +0530

    Q3 Arrays, similar to trapping rainwater problem

--- Arrays/Q3.cpp
+++ Arrays/Q3.cpp
@@ -3,13 +3,31 @@
 using namespace std ;
 
 int main(){
-    int n, air=0;
+
+    int n ;
     cin>>n;
     int arr[n];
-    for(int i = 0 ; i < n ; i++){
+    for(int i = 0 ; i <n ; i++){
         cin>>arr[i];
-        air++;
     }
-    cout<<air;
+    int res = 0 ;
+
+
+// For every element of the array
+    for(int i = 1 ; i <n - 1 ; i++ ){
+        int left = arr[i];       
+        //Finding the maximum element on the left of the index 
+        for(int j = 0 ; j<i ; j++){
+            left = max(left, arr[j]);
+        }
+        int right = arr[i];
+        // FInding the maximum element on the right of the index
+        for(int j = i+1 ; j<n ; j++){
+            right = max(right , arr[j]);
+        }
+        // Updating the max amount of air as the air accumulated by any block will be equal to the minimum height of the left and right block - the height of the own block  
+        res = res + (min(left,right) - arr[i]);
+    }
+    cout<<res;
     return 0;
 }
