73509bd3bcb55bbf2a7ae1dbe4516b8c700a1cf4
shubhraagarwal <agarwalshubhra2002@gmail.com>
2021-04-22 17:45:29 +0530

    Q4 Array

---
+++ Arrays/Q4.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std ;
+
+int main(){
+    int size, window ;
+    cin >> size ;
+    int arr[size];
+    for(int i = 0 ; i<size ; i++){
+        cin>>arr[i];
+    }
+    cin>>window;
+    for( int i = 0 ; i<= size ; i++){
+        bool flag ;
+
+        for(int j = i ; j < i+ window ; j++){
+            if(arr[j] < 0){
+                if( i > 0){
+                    cout << " ";
+                }
+                cout<< arr[j];
+                flag = true ;
+                break;
+            }
+
+        }
+        if(!flag){
+            cout<<"0 ";
+        }
+
+        
+        if(i >= size-window){
+            break;
+        }
+    }
+
+}
--- Arrays/tempCodeRunnerFile.cpp
+++
@@ -1,3 +0,0 @@
-for ( int i = 0 ; i < m+n ; i++){
-        cout<<arr3[i];
-    }
